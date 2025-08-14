879b299a2f29298fb613a886ba11c330f9eef414
NekitaSokol <sleepingcat716@gmail.com>
2025-08-14 21:46:05 +0500

    task2.1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,11 +7,19 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6); // your code
+let dice2 =Math.floor(Math.random() * 6);// your code
+let isWinningDouble; // your code
+ if ((dice1 && dice2 === dice1 && dice2) > 3) {
+  console.log(dice1,dice2)
+isWinningDouble = true;
+console.log(isWinningDouble)
+ } else {
+  console.log(dice1,dice2)
+  isWinningDouble = false
+  console.log(isWinningDouble)
+ }
 
-/*
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -19,4 +27,4 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
