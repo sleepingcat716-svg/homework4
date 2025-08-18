2dcedbde33581b44ee8db15bab688cae9c1f783a
NekitaSokol <sleepingcat716@gmail.com>
2025-08-18 23:20:49 +0500

    taska333.1

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,13 +2,10 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(num) {
-  if (num%2 === 1){
-   return false
-  } else if (num%2 === 0) { 
-   return true
-  }
-     
-}
+   return num%2 === 0
+   }
+
+console.log(isEven(2))
 
 
 
