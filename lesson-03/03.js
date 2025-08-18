0779dbc23e1f2b54767d5e8ac250810961bb8cb2
NekitaSokol <sleepingcat716@gmail.com>
2025-08-18 22:30:06 +0500

    taska3.3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,12 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(Number1, Number2, Number3) {
+    if (Number1 > Number2 && Number1 > Number3 || Number1 === Number2 && Number1 === Number3 ||
+         Number1 === Number2 && Number1 > Number3 ) {
+        console.log(Number1)
+    } else if (Number2 > Number1 && Number2 > Number3 || Number2 === Number3 && Number2 > Number1) {
+        console.log(Number2)
+    } else if (Number3 > Number1 && Number3 > Number2 || Number3 === Number1 && Number3 > Number2) {
+        console.log(Number3)
+    } 
+}
