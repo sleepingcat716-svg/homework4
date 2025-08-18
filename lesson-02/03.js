e55ee293d19db73a0f8eded4f912de976358cbf9
NekitaSokol <sleepingcat716@gmail.com>
2025-08-18 20:46:38 +0500

    task2.3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,33 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 100 // тестовое значение, можно изменять
 let grade
+const maxPointsA = 100;
+const minPointsA = 90;
+const maxPointsB = 89;
+const minPointsB = 80;  
+const maxPointsC = 79;
+const minPointsC = 70;
+const maxPointsD = 69;
+const minPointsD = 50
+const theAssignmentWasFailed = 49
+if (score >= minPointsA && score <= maxPointsA){
+    grade = "A";
+    console.log(grade);
+} else if (score >= minPointsB && score <= maxPointsB) {
+    grade = 'B';
+    console.log(grade);
+}
+    else if (score >= minPointsC && score <= maxPointsC) {
+        grade = 'C';
+        console.log(grade);
+    } else if (score >= minPointsD && score <= maxPointsD ) {
+        grade = 'D';
+        console.log(grade)
+    } else  if(score <= theAssignmentWasFailed ) {
+        grade = 'F'
+        console.log(grade)
+    } 
+
 // your code
