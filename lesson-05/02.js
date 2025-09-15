547266992729fbf02454d7228aa67f74bac099ff
NekitaSokol <sleepingcat716@gmail.com>
2025-09-15 21:33:50 +0500

    taska5.2.0

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -1,11 +1,15 @@
 /*
-Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
+Напишите функцию `updateGallery`,
+ которая будет обновлять информацию о произведениях искусства в виртуальной галерее.
+  Функция должна принимать три параметра:
 
 - объект галереи
 - название произведения (ключ)
 - новое значение
 
-Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
+Если произведение с таким названием уже есть в галерее, 
+его значение должно быть обновлено. Если произведения нет,
+ оно должно быть добавлено в объект галерею.
 
 Пример использования функции:
 
@@ -35,4 +39,18 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery1, objektGallery, titlOfTheWork) {
+const newGallerykey = Object.keys(gallery);
+
+for (let i = 0; i < newGallerykey.length; i++ ){
+  if (objektGallery === newGallerykey[i]){
+   gallery[objektGallery] = titlOfTheWork;
+  } else {gallery[objektGallery] = titlOfTheWork;
+  }
+  }
+}
+
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+console.log(gallery)
